b8d90be3a2e41455416354a0350305cceb53e943
cuger <49472930+zetrun-liu@users.noreply.github.com>
2019-05-24 22:37:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from model import unet, segnet_vgg16, fcn_vgg16_8s, VGGUnet2, res_unet, res_unet1, D_resunet, D_resunet1
+from model import unet, segnet_vgg16, fcn_vgg16_8s, VGGUnet2, res_unet, D_resunet, D_resunet1
 from data import trainGenerator, testGenerator, saveResult, testGenerator2
 from keras.callbacks import ModelCheckpoint, Callback, EarlyStopping, ReduceLROnPlateau, TensorBoard
 import keras.backend as K
@@ -17,32 +17,32 @@ data_gen_args = dict(rotation_range=90.,
                     #height_shift_range=0.1,
                     #shear_range=0.1,
                     #zoom_range=0.1,
+                    fill_mode='nearest'
                     horizontal_flip=True,
-                    vertical_flip=True,
-                    fill_mode='nearest')
+                    vertical_flip=True)
 
 train_Gene = trainGenerator(8,'data/membrane/train','image_crops','mask_crops',data_gen_args,save_to_dir = None)
 val_Gene = trainGenerator(8,'data/membrane/test','images_crops','masks_crops',data_gen_args)
 
-reduce_lr = ReduceLROnPlateau(monitor = 'val_loss', factor=0.2, patience=1, verbose=0, mode='min', epsilon=1e-4, 
+reduce_lr = ReduceLROnPlateau(monitor = 'val_loss', factor=0.2, patience=3, verbose=0, mode='min', epsilon=1e-4, 
                               cooldown=0, min_lr=1e-6)
 visual = TensorBoard(log_dir='./D_resunet1_log', histogram_freq=0, write_graph=True, write_images=True)
-earlystop = EarlyStopping(monitor='val_loss', patience=4, verbose=0, mode='min')
+earlystop = EarlyStopping(monitor='val_loss', patience=7, verbose=0, mode='min')
 #model = unet()
 #model = segnet_vgg16()
 #model = fcn_vgg16_8s()
 #model.load_weights('fcn_vgg16_8s.hdf5')
 #model = fcn_vgg16_8s()
 #model = VGGUnet2()
-model = D_resunet1()
+model = D_resunet()
 
 #model = res_unet1()
 #model.load_weights('res_unet.hdf5')
 
-model_checkpoint = ModelCheckpoint('D_Resunet1.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(train_Gene,steps_per_epoch=15000,epochs=50,
+model_checkpoint = ModelCheckpoint('D_Resunet.hdf5', monitor='val_loss',verbose=1, save_best_only=True)
+model.fit_generator(train_Gene,steps_per_epoch=3735,epochs=50,
                     callbacks=[model_checkpoint, visual, reduce_lr, earlystop], 
-                    validation_data=val_Gene, validation_steps=1560)#step_per_epoch and validation_steps equals to number of samples divide batchsize
+                    validation_data=val_Gene, validation_steps=220)#step_per_epoch and validation_steps equals to number of samples divide batchsize
 
 
 '''
